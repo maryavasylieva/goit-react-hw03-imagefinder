7cd1e645a17e0474109d665dbb9e6afff6240134
maryavasylieva <miheta.laurelin@gmail.com>
2019-08-05 02:02:51 +0300

    Updates

---
+++ precache-manifest.2ef479cc9384c8767b187c24e23c6e74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62572e51648f5916f44e2cb6dc400423",
+    "url": "/goit-react-hw03-imagefinder/index.html"
+  },
+  {
+    "revision": "acb161d45027e7d4db67",
+    "url": "/goit-react-hw03-imagefinder/static/css/main.9743b570.chunk.css"
+  },
+  {
+    "revision": "9f239a94e11334fd4eaa",
+    "url": "/goit-react-hw03-imagefinder/static/js/2.483d3adc.chunk.js"
+  },
+  {
+    "revision": "acb161d45027e7d4db67",
+    "url": "/goit-react-hw03-imagefinder/static/js/main.92267562.chunk.js"
+  },
+  {
+    "revision": "7347621b6264f8889469",
+    "url": "/goit-react-hw03-imagefinder/static/js/runtime~main.0eae0247.js"
+  }
+]);
